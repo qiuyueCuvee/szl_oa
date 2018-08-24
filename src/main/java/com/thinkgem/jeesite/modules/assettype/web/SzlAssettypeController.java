5870c5abd6b039900ecd4206d6c821c6326aec1f
fangbaosheng <baosheng.fangdeeinfar.com>
2018-08-24 16:45:10 +0800

    remove 多余代

--- src/main/java/com/thinkgem/jeesite/modules/assettype/web/SzlAssettypeController.java
+++ src/main/java/com/thinkgem/jeesite/modules/assettype/web/SzlAssettypeController.java
@@ -10,11 +10,9 @@ import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.thinkgem.jeesite.common.config.Global;
@@ -69,17 +67,6 @@ public class SzlAssettypeController extends BaseController {
 		return "modules/assettype/szlAssettypeForm";
 	}
 
-	/*@RequiresPermissions("assettype:szlAssettype:edit")
-	@RequestMapping(value = "save")
-	public String save(SzlAssettype szlAssettype, Model model, RedirectAttributes redirectAttributes) {
-		if (!beanValidator(model, szlAssettype)){
-			return form(szlAssettype, model);
-		}
-		szlAssettypeService.save(szlAssettype);
-		addMessage(redirectAttributes, "保存assettype成功");
-		return "redirect:"+Global.getAdminPath()+"/assettype/szlAssettype/?repage";
-	}*/
-	
 	/**
 	 * 保存assettype
 	 */
